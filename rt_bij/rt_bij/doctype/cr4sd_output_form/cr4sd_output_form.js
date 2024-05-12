d4ddf9605fdb94655593dcd271e3258a5f912c98
aniketsuvaidyam <aniket.suvaidyam@gmail.com>
2024-05-12 16:58:16 +0530

    add form c4output form

--- rt_bij/rt_bij/doctype/cr4sd_output_form/cr4sd_output_form.js
+++ rt_bij/rt_bij/doctype/cr4sd_output_form/cr4sd_output_form.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, suvaidyam and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Approvals", {
+// frappe.ui.form.on("CR4SD Output Form", {
 // 	refresh(frm) {
 
 // 	},
