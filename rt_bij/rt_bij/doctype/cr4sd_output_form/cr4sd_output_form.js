8c320aa72ad52d745c48e9a2f619264114aefe39
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-05-13 18:12:33 +0530

    added validaction in CR4SD Output Form

--- rt_bij/rt_bij/doctype/cr4sd_output_form/cr4sd_output_form.js
+++ rt_bij/rt_bij/doctype/cr4sd_output_form/cr4sd_output_form.js
@@ -18,6 +18,11 @@ function apply_filter(field_name, filter_on, frm, filter_value, withoutFilter = 
         };
     }
 };
+const integer_length_validator = (value, reqd_length, label) => {
+    if (value && String(value).length > reqd_length) {
+        frappe.throw(`Count of ${label} can't be more than ${reqd_length} digits!`)
+    }
+}
 frappe.ui.form.on("CR4SD Output Form", {
     refresh(frm) {
         apply_filter("district", "state", frm, frm.doc.state)
@@ -25,10 +30,7 @@ frappe.ui.form.on("CR4SD Output Form", {
         apply_filter("talukatehsil", "block", frm, frm.doc.block)
         apply_filter("gram_panchayat", "grampanchayat", frm, frm.doc.talukatehsil)
         apply_filter("village", "village", frm, frm.doc.gram_panchayat)
-
-        // console.log("refreshed",frm.doc)
     },
-
     state: function (frm) {
         apply_filter("district", "state", frm, frm.doc.state)
     },
@@ -47,29 +49,58 @@ frappe.ui.form.on("CR4SD Output Form", {
     villages: function (frm) {
         apply_filter("village", "village", frm, frm.doc.gram_panchayat)
     },
-    // before_save(frm) {
-    //     if (frm.doc.phone_numbers_of_the_sarpanch == "+91-") {
-    //         frm.doc.phone_numbers_of_the_sarpanch = "";
-    //     }
-    //     if (frm.doc.phone_numbers_of_the_sarpanch2 == "+91-") {
-    //         frm.doc.phone_numbers_of_the_sarpanch2 = "";
-    //     }
-    //     if (frm.doc.phone2 == "+91-") {
-    //         frm.doc.phone2 = "";
-    //     }
-    //     if (frm.doc.phone3 == "+91-") {
-    //         frm.doc.phone3 = "";
-    //     }
-    //     if (frm.doc.phone4 == "+91-") {
-    //         frm.doc.phone4 = "";
-    //     }
-    // },
     before_save(frm) {
-        ["phone_numbers_of_the_sarpanch", "phone_numbers_of_the_sarpanch2", "phone2", "phone3", "phone4"].forEach(field => {
+        ["phone_numbers_of_the_sarpanch", "phone_numbers_of_the_sarpanch2", "phone", "phone2", "phone3", "phone4"].forEach(field => {
             if (frm.doc[field] === "+91-") {
                 frm.doc[field] = "";
             }
         });
-    }
+    },
+    validate(frm) {
+        integer_length_validator(frm.doc.leverage_amount, 7, 'Leverage Amount');
+        integer_length_validator(frm.doc.leverage_amount2, 7, 'Leverage Amount');
+        integer_length_validator(frm.doc.extent_of_area_where_the_practices_adopted, 7, 'Extent of area where the practices adopted');
+        integer_length_validator(frm.doc.leverage_amount_23, 10, 'Leverage Amount (2.3)');
+        integer_length_validator(frm.doc.increased_area, 7, 'Increased area');
+        integer_length_validator(frm.doc.leverage_amount_24, 7, 'Leverage Amount (2.4)');
+        integer_length_validator(frm.doc.estimated_number_of_households_benefitted, 5, 'Estimated number of Households benefitted');
+        integer_length_validator(frm.doc.area_of_extent_climate_adaptive_farming_practices_taken_up, 5, 'Area of Extent climate adaptive farming practices taken up');
+        integer_length_validator(frm.doc.no_of_initiatives, 7, 'No. of Initiatives');
+        integer_length_validator(frm.doc.leverage_money, 10, 'Leverage Money');
+        integer_length_validator(frm.doc.area, 10, 'Area');
+    },
+    leverage_amount: function (frm) {
+        integer_length_validator(frm.doc.leverage_amount, 7, 'Leverage Amount');
+    },
+    leverage_amount2: function (frm) {
+        integer_length_validator(frm.doc.leverage_amount2, 7, 'Leverage Amount');
+    },
+    extent_of_area_where_the_practices_adopted: function (frm) {
+        integer_length_validator(frm.doc.extent_of_area_where_the_practices_adopted, 7, 'Extent of area where the practices adopted');
+    },
+    leverage_amount_23: function (frm) {
+        integer_length_validator(frm.doc.leverage_amount_23, 10, 'Leverage Amount (2.3)');
+    },
+    increased_area: function (frm) {
+        integer_length_validator(frm.doc.increased_area, 7, 'Increased area');
+    },
+    leverage_amount_24: function (frm) {
+        integer_length_validator(frm.doc.leverage_amount_24, 7, 'Leverage Amount (2.4)');
+    },
+    estimated_number_of_households_benefitted: function (frm) {
+        integer_length_validator(frm.doc.estimated_number_of_households_benefitted, 5, 'Estimated number of Households benefitted');
+    },
+    area_of_extent_climate_adaptive_farming_practices_taken_up: function (frm) {
+        integer_length_validator(frm.doc.area_of_extent_climate_adaptive_farming_practices_taken_up, 5, 'Area of Extent climate adaptive farming practices taken up');
+    },
+    no_of_initiatives: function (frm) {
+        integer_length_validator(frm.doc.no_of_initiatives, 7, 'No. of Initiatives');
+    },
+    leverage_money: function (frm) {
+        integer_length_validator(frm.doc.leverage_money, 10, 'Leverage Money');
+    },
+    area: function (frm) {
+        integer_length_validator(frm.doc.area, 10, 'Area');
+    },
 
 });
