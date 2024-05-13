188489ac17b2338159e2419e610784f06fb0a646
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-05-13 11:52:36 +0530

    add:

--- rt_bij/rt_bij/doctype/ivcd_output_form/ivcd_output_form.js
+++ rt_bij/rt_bij/doctype/ivcd_output_form/ivcd_output_form.js
@@ -1,7 +1,27 @@
 // Copyright (c) 2024, suvaidyam and contributors
 // For license information, please see license.txt
+function apply_filter(field_name, filter_on, frm, filter_value, withoutFilter = false) {
+    frm.fields_dict[field_name].get_query = () => {
+        if (withoutFilter) {
+            return {
+                filters: {},
+                page_length: 1000
+            };
+        }
+        return {
+            filters: {
+                [filter_on]: filter_value || frm.doc[filter_on] || `please select ${filter_on}`,
+            },
+            page_length: 1000
+        };
+    }
+};
 
 frappe.ui.form.on("IVCD Output Form", {
     refresh(frm) {
+        apply_filter("district", "state", frm, frm.doc.state)
+        apply_filter("block", "district", frm, frm.doc.district)
+        apply_filter("grampanchayat", "block", frm, frm.doc.block)
+        apply_filter("village", "grampanchayat", frm, frm.doc.grampanchayat)
     },
 });
