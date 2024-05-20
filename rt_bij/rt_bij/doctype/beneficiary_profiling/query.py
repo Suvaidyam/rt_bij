17639e0ab8584c8d1f957eaa9c27a02c4df9078b
aniketsuvaidyam <aniket.suvaidyam@gmail.com>
2024-05-20 18:40:11 +0530

    remove beneficiary profiling

--- rt_bij/rt_bij/doctype/beneficiary_profiling/query.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-def list_query(user):
-    if not user:
-        user = frappe.session.user
-    if "State User" in frappe.get_roles(user) and ("Administrator" not in frappe.get_roles(user)):
-        usr =  frappe.get_doc('User', user)
-        if usr is not None:
-            return """district_level_status = 'Accepted'"""
