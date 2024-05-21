1afdbd3547155bbcb07afab6d76856fedda8c7eb
Sujeet Yadav <sujeetyadav.suvaidyam@gmail.com>
2024-05-21 12:11:48 +0530

    something add css

--- rt_bij/rt_bij/doctype/ivcd_output_form/ivcd_output_form_list.js
+++ rt_bij/rt_bij/doctype/ivcd_output_form/ivcd_output_form_list.js
@@ -1,3 +1,4 @@
+let data;
 function convertTimestampToPassedTime(timestamp) {
     const currentTime = moment();
     const timestampMoment = moment(timestamp);
@@ -16,24 +17,40 @@ function convertTimestampToPassedTime(timestamp) {
         return months === 1 ? '1 month ago' : `${months} months ago`;
     }
 }
-
-frappe.listview_settings['IVCD output form'] = {
+function formatDate(timestamp) {
+    const date = moment(timestamp);
+    return date.format('DD MMMM, YYYY');
+}
+frappe.listview_settings['CRS4D activity form'] = {
     refresh: function (listview) {
         $("use.like-icon").hide();
         $(".comment-count").hide();
         $(".frappe-timestamp").hide();
         $(".avatar-small").hide();
+        for (let item of data) {
+            var pendingSinceElement = document.querySelectorAll(`.list-row-col.ellipsis.hidden-xs [title="Pending Since: ${item.pending_since}"]`);
+            if (item.pending_since !== "-") {
+                pendingSinceElement.forEach((element) => {
+                    element.style.backgroundColor = '#FBE3D8';
+                    element.style.borderRadius = '20%';
+                    element.style.padding = '6px 12px';
+                })
+            }
+        }
     },
     onload: function (listview) {
         $('.layout-side-section').hide();
         listview.after_render = function () {
+
             let new_data = listview.data.map((item) => {
                 return {
-                    ...item, date_of_visit: frappe.datetime.str_to_user(item.creation).split(" ")[0],
+                    ...item, date_of_visit: formatDate(item.creation),
                     pending_since: ["Pending at DPM", "Pending at SPM"].includes(item.workflow_state) ? convertTimestampToPassedTime(item.modified) : "-"
                 }
             })
+            data = new_data
             listview.data = new_data;
+
             listview.render_list();
         }
     }
